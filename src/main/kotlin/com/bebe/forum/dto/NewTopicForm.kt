132623e2f8cd2eef17dca3e12b3b0caa9d274c3d
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-09-06 20:49:24 -0300

    Adding validations in post request.

--- src/main/kotlin/com/bebe/forum/dto/NewTopicForm.kt
+++ src/main/kotlin/com/bebe/forum/dto/NewTopicForm.kt
@@ -1,9 +1,13 @@
 package com.bebe.forum.dto
 
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
+
 data class NewTopicForm(
     var id: Long?,
-    val title: String,
-    val message: String,
-    val idCourse: Long,
-    val idUser: Long
+    @field:NotEmpty @field:Size(min = 5, max = 100) val title: String,
+    @field:NotEmpty val message: String,
+    @field:NotNull val idCourse: Long,
+    @field:NotNull val idUser: Long
 )
