38c26c49ffe666597fd3c5988d184a0e5a2b8e07
BrunoMCerqueira <brunocerqueirasd@gmail.com>
2022-09-16 20:18:14 -0300

    Add Bad request error handler.

--- src/main/kotlin/com/bebe/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/bebe/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.bebe.forum.exception
 
 import com.bebe.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -15,6 +16,21 @@ class ExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     fun handleNotFound(exception: NotFoundException, request: HttpServletRequest): ErrorView {
         return ErrorView(status = HttpStatus.NOT_FOUND.value(), error = HttpStatus.NOT_FOUND.name, message = exception.message!!, path = request.servletPath)
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(exception: MethodArgumentNotValidException, request: HttpServletRequest): ErrorView {
+        val errorMessage = HashMap<String, String?>()
+        exception.bindingResult.fieldErrors.forEach {
+            e -> errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
+        )
     }
 
     @ExceptionHandler(Exception::class)
